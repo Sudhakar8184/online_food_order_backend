5a4c9c9efc191537d1dfb6957a836117fd1e5552
Sudhakar8184 <chintareddy.sudhakarreddy@gmail.com>
2019-05-18 17:17:01 +0530

    new changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./server/init');
 const http = require('http');
 const server = http.createServer(app);
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 server.listen(port);
 console.log("server started ......")
